0b986768ea6d766adfff71e1a7a0ad57fb47c355
unknown <jithinbp007@gmail.com>
2024-03-13 17:10:42 +0530

    updating navbar toggle

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
 import "./sass/style.scss";
+
+const navMenu = document.querySelector(".header__navigation");
+const menuBtn = document.querySelector(".btn--menu");
+
+menuBtn.addEventListener("click", () => {
+  const visibility = navMenu.getAttribute("data-visible");
+
+  if (visibility === "false") {
+    navMenu.setAttribute("data-visible", "true");
+    menuBtn.setAttribute("aria-expanded", "true");
+  } else {
+    navMenu.setAttribute("data-visible", "false");
+    menuBtn.setAttribute("aria-expanded", "false");
+  }
+});
