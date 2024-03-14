198095f6873493a1c101a6657e891478004b6115
unknown <jithinbp007@gmail.com>
2024-03-14 15:08:13 +0530

    updated body scroll off during menu open

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ import "./sass/style.scss";
 
 const navMenu = document.querySelector(".header__navigation");
 const menuBtn = document.querySelector(".btn--menu");
+const body = document.querySelector("body");
 
 menuBtn.addEventListener("click", () => {
   const visibility = navMenu.getAttribute("data-visible");
@@ -9,8 +10,10 @@ menuBtn.addEventListener("click", () => {
   if (visibility === "false") {
     navMenu.setAttribute("data-visible", "true");
     menuBtn.setAttribute("aria-expanded", "true");
+    body.classList.add("no-scroll");
   } else {
     navMenu.setAttribute("data-visible", "false");
     menuBtn.setAttribute("aria-expanded", "false");
+    body.classList.remove("no-scroll");
   }
 });
